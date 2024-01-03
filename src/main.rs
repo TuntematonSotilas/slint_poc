477d6fcbb6a9d3ba085bb5d4c5b8d57fd5601ccb
Gaël <gael.batault@arche-mc2.fr>
2024-01-03 16:02:55 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@ fn main() {
     MainWindow::new().unwrap().run().unwrap();
 }
 
+
 slint::slint! {
-    export component MainWindow inherits Window {
+
+    export struct Palette {
+        gradient: brush
+    }
+
+    export global Theme {
+        in property <Palette> palette: {
+            gradient: @linear-gradient(135deg, #80BCBD 0%, #AAD9BB 100%),
+        };
+    }
+
+    component Tile inherits Rectangle {
+        background: Theme.palette.gradient;
         Text {
             text: "hello world";
-            color: green;
+            color: #F9F7C9;
         }
+    }
+
+    export component MainWindow inherits Window {
+        width: 400px;
+        height: 400px;
+        Tile {}
     }
 }
